3a2d6cb68f78c71aec2b0366c366af694dd16062
Vitor Melo <20214218-VitorMelo15@users.noreply.replit.com>
2024-09-28 01:46:29 +0000

    clean project

--- src/domain/interfaces/user.model.interface.ts
+++
@@ -1,7 +0,0 @@
-import { IUser } from './user.interface';
-
-export const USER_MODEL = 'IUserModel';
-export interface IUserModel {
-  create(user: IUser): Promise<IUser>;
-  findByName(name: string): Promise<IUser[]>;
-}
--- src/domain/interfaces/user.service.interface.ts
+++
@@ -1,8 +0,0 @@
-import { IUser } from './user.interface';
-
-export const USER_SERVICE = 'IUserService';
-
-export interface IUserService {
-  create(user: IUser): Promise<IUser>;
-  findByName(name: string): Promise<IUser[]>;
-}
